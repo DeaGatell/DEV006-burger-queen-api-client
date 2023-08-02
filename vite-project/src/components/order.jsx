047c0f0890e3c3a0ab2a5d7833767c239d2c2d2e
Dea Gatell <deagatell6@gmail.com>
2023-08-01 20:27:23 -0500

    Nuevo archivo de llamado de data

--- vite-project/src/components/order.jsx
+++ vite-project/src/components/order.jsx
@@ -16,57 +16,63 @@ function ProductsData() {
     }, []);
 
     return (
-        <div className=''>
+        <div className='grid grid-cols-2 gap-3'>
             {productsData.map((product) => (
                 <div
                     key={product.id}
                     //onClick={() => handleProductClick(product.price)}
-                    className='border-[#A1D2B5] border-[3px] rounded-[50px] w-[250px] h-[130px] justify-center my-3 mx-5 font-judson text-2xl p-2 shadow-lg'
+                    className='bg-lime-500 border-amber-500 border-[3px] rounded-[50px] my-3 mx-5 font-judson text-2xl p-2 shadow-lg'
                 >
-                    <img src={product.image} alt='Latte' className='rounded-xl' />
-                    <p className=''>{product.name}</p>
-                    <p className=''>$ {product.price}</p>
+                    <div className='flex items-center justify-center'>
+                        <img
+                            src={product.image}
+                            alt='Latte'
+                            className='w-12 h-12 rounded-xl'
+                        />
+                    </div>
+                    <p className='text-xl text-center text-white'>{product.name}</p>
+                    <p className='text-xl text-center text-white'>$ {product.price}</p>
                 </div>
             ))}
         </div>
     );
 }
 
-    // const handleProductClick = (price) => {
-    //     setTotalPrice(totalPrice + price);
-    // }
+// const handleProductClick = (price) => {
+//     setTotalPrice(totalPrice + price);
+// }
 
-    // function FilterProducts() {
-    //     //Filtrar productos según el valor de selectedItem
-    //     let filteredProducts = [];
+// function FilterProducts() {
+//     //Filtrar productos según el valor de selectedItem
+//     let filteredProducts = [];
 
-    //     if (selectedItem === 'desayuno') {
-    //         filteredProducts = productsData.filter(product => product.type === 'Desayuno');
-    //     } else if (selectedItem === 'almuerzo') {
-    //         filteredProducts = productsData.filter(product => product.type === 'Almuerzo');
-    //     }
+//     if (selectedItem === 'desayuno') {
+//         filteredProducts = productsData.filter(product => product.type === 'Desayuno');
+//     } else if (selectedItem === 'almuerzo') {
+//         filteredProducts = productsData.filter(product => product.type === 'Almuerzo');
+//     }
 
-    //     return filteredProducts.map(product => (
-    //         <div className='' key={product.id}>
-    //             <button 
-    //             onClick={() => handleProductClick(product.price)}
-    //             className='border-[#A1D2B5] border-[3px] rounded-[50px] w-[250px] h-[130px] justify-center my-3 mx-5 font-judson text-2xl p-2 shadow-lg'>
-    //             <p className=''>{product.name}</p>
-    //             <p className=''>$ {product.price}</p>
-    //             </button>
-    //         </div>
-    //     ));
-    // }
+//     return filteredProducts.map(product => (
+//         <div className='' key={product.id}>
+//             <button 
+//             onClick={() => handleProductClick(product.price)}
+//             className='border-[#A1D2B5] border-[3px] rounded-[50px] w-[250px] h-[130px] justify-center my-3 mx-5 font-judson text-2xl p-2 shadow-lg'>
+//             <p className=''>{product.name}</p>
+//             <p className=''>$ {product.price}</p>
+//             </button>
+//         </div>
+//     ));
+// }
 
-    //return (
-    //<div>
-    //<aside>
-    //{FilterProducts()}
-    //</aside>
-    //<aside> Total: ${totalPrice} </aside>
-    //</div>
-    //);
+//return (
+//<div>
+//<aside>
+//{FilterProducts()}
+//</aside>
+//<aside> Total: ${totalPrice} </aside>
+//</div>
+//);
 
 //}
 
-export { ProductsData } ;
+export { ProductsData };
